78cbdb24374301e7c42d8d71b3f1550a3844497f
Arjun Shrinivas <vmsnivas@gmail.com>
2017-07-31 12:01:16 +0530

    Modified vpc.tf

--- terraproject/wordpress/vpc.tf
+++ terraproject/wordpress/vpc.tf
@@ -123,3 +123,70 @@ resource "aws_route_table_association" "rds_instance_group" {
 		Name = "rds_sng"
 	}
 }
+
+# Security Group - PUBLIC
+resource "aws_security_group" "public" {
+	name = "sg_public"
+	description = "Used for Public and Private instances for load balancer access"
+	vpc_id = "${aws_vpc.vpc.id}"
+
+	# SSH-INGRESS
+	ingress {
+		from_port	= 22
+		to_port		= 22
+		protocol	= "tcp"
+		cidr_blocks	= ["${var.localip}"]
+	}
+	
+	# HTTP-INGRESS
+	ingress {
+		from_port	= 80
+		to_port		= 80
+		protocol	= "tcp"
+		cidr_blocks	= ["0.0.0.0/0"]
+	}
+
+	# HTTP-EGRESS
+	egress {
+		from_port	= 0
+		to_port		= 0
+		protocol	= "-1"
+		cidr_blocks	= ["0.0.0.0/0"]
+	}
+}
+
+# Security Group - PRIVATE
+resource "aws_security_group" "private" {
+	name = "sg_private"
+	description = "Used for private instances"
+	vpc_id = "${aws_vpc.vpc.id}"
+	
+	# Access from other security groups
+	ingress {
+		from_port	= 0
+		to_port		= 0
+		protocol	= "-1"
+		cidr_blocks	= ["10.1.0.0/16"]
+	}
+	egress {
+		from_port	= 0
+		to_port		= 0
+		protocol	= "-1"
+		cidr_blocks	= ["0.0.0.0/0"]
+	}
+}
+
+# Security Group - RDS
+resource "aws_security_group" "rds" {
+	name = "sg_rds"
+	description = "Used for DB instances"
+	vpc_id = "${aws_vpc.vpc.id}"
+
+	# SQL access from public/private security group
+	ingress {
+		from_port	= 3306
+		to_port		= 3306
+		protocol	= "tcp"
+		security_groups	= ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
+	}
+}
