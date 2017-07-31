325eeb9f8a73e42b089144d7b40a99991b5b76f3
Arjun Shrinivas <vmsnivas@gmail.com>
2017-07-31 17:12:41 +0530

    Added terraproject/wordpress/dev.tf

---
+++ terraproject/wordpress/dev.tf
@@ -0,0 +1,24 @@
+# Create dev instance
+resource "aws_instance" "dev" {
+        instance_type = "${var.dev_instance_type}"
+        ami = "${var.dev_ami}"
+        tags {
+                Name = "dev"
+        }
+        key_name = "${aws_key_pair.auth.id}"
+        vpc_security_group_ids = ["${aws_security_group.public.id}"]
+        iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
+        subnet_id = "${aws_subnet.public.id}"
+        provisioner "local-exec" {
+                command = "cat <<EOF > aws_hosts
+[dev]
+${aws_instance.dev.public_ip}
+[dev:vars]
+s3code=${aws_s3_bucket.code.bucket}
+EOF"
+        }
+        provisioner "local-exec" {
+                command = "sleep 6m && ansible-playbook -i aws_hosts wordpress.yml"
+        }
+}
+
