98db7144548514a26940624347af13931683979e
Arjun Shrinivas <vmsnivas@gmail.com>
2017-08-07 14:17:40 +0530

    Modified 00-provider.tf

--- terraproject/lamptest/00-provider.tf
+++ terraproject/lamptest/00-provider.tf
@@ -1,6 +1,8 @@
 # AWS PROVIDER CONFIG
 
 provider "aws" {
-	region	= "us-west-2"
-	profile	= "lldev01"
+#	region	= "us-west-2"
+#	profile	= "lldev01"
+	region	= "${var.aws_region}"
+	profile	= "${var.aws_profile}"
 }
