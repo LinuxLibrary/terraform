25bac9160843232d54cba527afa8965003b3cb5a
Arjun Shrinivas <vmsnivas@gmail.com>
2019-08-18 14:31:24 +0530

    Reset HEAD prior to mesos manifests

--- Manifests/mesos/00DynamicValues.tf
+++
@@ -1,6 +0,0 @@
-# Resource to get the local ip. This is needed to get access
-resource "null-resource" "localip" {
-	provisioner "local-exec" {
-		command = "echo $(dig +short myip.opendns.com @resolver1.opendns.com)"
-	}
-}
--- Manifests/mesos/01VPC.tf
+++
@@ -1,7 +0,0 @@
-# VPC CIDR-Block
-resource "aws_vpc" "vpc" {
-	cidr_block = "10.1.0.0/16"
-	tags {
-		Name = "hawkeye"
-	}
-}
