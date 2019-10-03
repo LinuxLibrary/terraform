554ae2b113afc3a0fd0b980f0e0631fc7dd611c1
Arjun Shrinivas <vmsnivas@gmail.com>
2019-10-04 00:31:49 +0530

    Added Manifests/dockerinfra/03AnsibleServer.tf

---
+++ Manifests/dockerinfra/03AnsibleServer.tf
@@ -0,0 +1,16 @@
+## Create Ansible Server
+#resource "aws_instance" "ansible_server" {
+#	tags = {
+#		Name = "AnsibleServer"
+#	}
+#
+#	ami = "${var.ami}"
+#	instance_type = "${var.instance_type}"
+#	key_name = "${aws_key_pair.ansible.key_name}"
+#	vpc_security_group_ids = ["${aws_security_group.public.id}"]
+#	subnet_id = "${aws_subnet.public.id}"
+#
+#	provisioner "local-exec" {
+#		command = "sleep 6m && apt-get update -y && apt-get install python python-pip git -y && pip install ansible-y"
+#	}
+#}
