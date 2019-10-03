0a9fdabd2d8b51416a18713dae47ed0728644347
Arjun Shrinivas <vmsnivas@gmail.com>
2019-10-04 00:31:49 +0530

    Added Manifests/dockerinfra/02KeyPair.tf

---
+++ Manifests/dockerinfra/02KeyPair.tf
@@ -0,0 +1,10 @@
+# Key Pair
+resource "aws_key_pair" "docker" {
+	key_name	= "${var.docker_key_name}"
+	public_key	= "${file(var.docker_key_path)}"
+}
+
+resource "aws_key_pair" "ansible" {
+	key_name 	= "${var.ansible_key_name}"
+	public_key	= "${file(var.ansible_key_path)}"
+}
