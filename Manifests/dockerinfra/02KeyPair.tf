55b334773eb14168ce68f393971cae35f88f0236
Arjun Shrinivas <vmsnivas@gmail.com>
2019-10-07 00:43:50 +0530

    Modified Manifests/dockerinfra/02KeyPair.tf

--- Manifests/dockerinfra/02KeyPair.tf
+++ Manifests/dockerinfra/02KeyPair.tf
@@ -1,4 +1,9 @@
 # Key Pair
+resource "aws_key_pair" "geekhost" {
+	key_name	= "${var.geekhost}"
+	public_key	= "${file(var.geekhost_path)}"
+}
+
 resource "aws_key_pair" "docker" {
 	key_name	= "${var.docker_key_name}"
 	public_key	= "${file(var.docker_key_path)}"
