f30b7f3a501170c8f9f79d02e48ff7345f788f35
Arjun Shrinivas <vmsnivas@gmail.com>
2020-03-28 21:26:58 +0530

    Modified Manifests/ansible/02KeyPair.tf

--- Manifests/ansible/02KeyPair.tf
+++ Manifests/ansible/02KeyPair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "instance" {
-	key_name = "${var.instance_key_name}"
-	public_key = "${file(var.instance_key_path)}"
+  key_name   = "${var.instance_key_name}"
+  public_key = "${file(var.instance_key_path)}"
 }
