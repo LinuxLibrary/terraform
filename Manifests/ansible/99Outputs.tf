e110d643b1dc0477e3428ef32a4de9b18f4c14ce
Arjun Shrinivas <vmsnivas@gmail.com>
2020-03-28 21:26:59 +0530

    Modified Manifests/ansible/99Outputs.tf

--- Manifests/ansible/99Outputs.tf
+++ Manifests/ansible/99Outputs.tf
@@ -1,7 +1,7 @@
 output "acs_public_ip" {
-	value = "${aws_instance.acs.public_ip}"
+  value = "${aws_instance.acs.public_ip}"
 }
 
 output "ansible_nodes_public_ips" {
-	value = "${aws_instance.ansible_nodes.*.public_ip}"
+  value = "${aws_instance.ansible_nodes.*.private_ip}"
 }
